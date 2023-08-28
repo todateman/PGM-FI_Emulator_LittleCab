e744e1caec9b25b8cef5fd015dee14899757f81b
todateman <todateman@yahoo.co.jp>
2023-08-28 23:44:45 +0900

    精度優先のためボリューム調整・回転数の出力を廃止

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,8 @@
 /**
 * PGM-FI Emulator
 */
- 
-#include <Arduino.h>
 
-// I2c外部ディスプレイの設定(A4:SDA A5:SCL)
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
- // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#include <Arduino.h>
 
 #define SERIAL_RATE 9600
 #define SERIAL_TIMEOUT 1000
@@ -24,12 +14,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define SENSOR_TA 6
 #define SENSOR_TO 9
 #define SENSOR_PB 10
-const int Volume_THL = A0;
 
 #define PCP 8
  
 const uint8_t analogOutPins[] = {3,5,6,9,10,11};
- 
+
+uint16_t RPM = 1700;
+uint16_t pulseHigh;
+uint16_t pulseLow;
+
 // forDebug
 static FILE uartout;
 static int uart_putchar(char c, FILE *stream) {
@@ -39,7 +32,7 @@ static int uart_putchar(char c, FILE *stream) {
     return -1;
   }
 }
- 
+
 void setup(){
   //forDebug
   fdev_setup_stream(&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
@@ -62,40 +55,18 @@ void setup(){
   analogWrite(SENSOR_TO, 138); //2.7-3.1V(138-158)
   analogWrite(SENSOR_PB, 138); //2.7-3.1V(138-158)
   
-  // I2C
-   Wire.begin();
-   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-     Serial.println(F("SSD1306 allocation failed"));
-     delay(3000);
-   }
+  pulseHigh = 10000000 / 12 / RPM;        //pulseHigh = 60 / RPM * 1000 / 12 * (5 / 30) * 1000;
+  pulseLow = 5000000 / RPM - pulseHigh;   //pulseLow = (60 / RPM * 1000 / 12) * 1000 - pulseHigh;
 }
 
 void loop(){
 
   uint8_t i = 0;
-  //uint32_t pulseHigh = 500;//1700RPM
-  //uint32_t pulseLow = 2500;//1700RPM
+  //pulseHigh = 500;  //1700RPM
+  //pulseLow = 2500;  //1700RPM
 
   //generate crank pulse
   while(1){
-    int Value_HTL = analogRead( Volume_THL );
-    int RPM = map(Value_HTL, 0, 1023, 0, 10000);
-
-    uint32_t pulseHigh = 60 / RPM * 1000 / 12 * (5 / 30);
-    uint32_t pulseLow =  60 / RPM * 1000 / 12 - pulseHigh;
-  
-    Serial.print(F("rpm: "));
-    Serial.println(RPM);
-
-    // I2c
-    display.clearDisplay();
-    display.setTextSize(2);             // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE);        // Draw white text
-    display.setCursor(0,0);             // Start at top-left corner
-    display.print(RPM);
-    display.println(F(" rpm"));
-    display.display();
-
     for(i=0; i<12; i++){
       if(i < 9){
         digitalWrite(PCP, HIGH);
