9ff25a4ace395fb37253de4e4909547784384085
todateman <todateman@yahoo.co.jp>
2023-08-30 02:14:21 +0900

    TaskSignal内の周期処理が未完成

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,15 @@
 /**
 * PGM-FI Emulator
 */
- 
 #include <Arduino.h>
 
 // マルチタスク
 #include <Arduino_FreeRTOS.h>
-void PCPOUT( void *pvParameters );	// 信号出力用ポインタ
-void DISPOUT( void *pvParameters );	// 表示用ポインタ
-
-// I2c外部ディスプレイの設定(A4:SDA A5:SCL)
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
- // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+void TaskAnalogRead( void *pvParameters );
+void TaskSignal( void *pvParameters );
+// キュー
+#include <queue.h>
+QueueHandle_t integerQueue;
 
 #define SERIAL_RATE 9600
 #define SERIAL_TIMEOUT 1000
@@ -35,9 +27,9 @@ const int Volume_THL = A0;
  
 const uint8_t analogOutPins[] = {3,5,6,9,10,11};
 
-volatile uint16_t RPM = 1700;
-volatile uint16_t pulseHigh;
-volatile uint16_t pulseLow;
+//volatile uint16_t RPM = 1700;
+//volatile uint16_t pulseHigh;
+//volatile uint16_t pulseLow;
 
 // forDebug
 static FILE uartout;
@@ -46,54 +38,6 @@ static int uart_putchar(char c, FILE *stream) {
     return 0;
   } else {
     return -1;
-  }
-}
- 
-
-void PCPOUT(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-  pinMode(PCP, OUTPUT); //CrankPulse
-  while(1){
-    volatile uint8_t i = 0;
-    for(i=0; i<12; i++){
-      if(i < 9){
-        digitalWrite(PCP, HIGH);
-      }
-      vTaskDelay( pulseHigh / portTICK_PERIOD_MS ); // wait
- 
-      if(i < 9){
-        digitalWrite(PCP, LOW);
-      }
-      vTaskDelay( pulseLow / portTICK_PERIOD_MS ); // wait
-    }
-  }
-}
-
-
-void DISPOUT(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-  while(1){
-    volatile uint16_t Value_HTL = analogRead( Volume_THL );
-    RPM = map(Value_HTL, 0, 1023, 0, 100) * 100;
-
-    pulseHigh = 10000 / 12 / RPM;        //pulseHigh = 60 / RPM * 1000 / 12 * (5 / 30) * 1000;
-    pulseLow = 5000 / RPM - pulseHigh;   //pulseLow = (60 / RPM * 1000 / 12) * 1000 - pulseHigh;
-  
-    Serial.print(F("rpm: "));
-    Serial.println(RPM);
-
-    // I2c
-    display.clearDisplay();
-    display.setTextSize(2);             // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE);        // Draw white text
-    display.setCursor(0,0);             // Start at top-left corner
-    display.print(RPM);
-    display.println(F(" rpm"));
-    display.display();
-    vTaskDelay( 500 / portTICK_PERIOD_MS );
   }
 }
 
@@ -110,7 +54,7 @@ void setup(){
   for(uint8_t i = 0; i<sizeof(analogOutPins); i++){
     pinMode(analogOutPins[i], OUTPUT);
   }
-  //pinMode(PCP, DISPOUT); //CrankPulse
+  //pinMode(PCP, OUTPUT); //CrankPulse
   
   // sensor initial value
   analogWrite(SENSOR_O2, 25); //0-1v(0-51)
@@ -119,70 +63,75 @@ void setup(){
   analogWrite(SENSOR_TO, 138); //2.7-3.1V(138-158)
   analogWrite(SENSOR_PB, 138); //2.7-3.1V(138-158)
   
-  // I2C
-  Wire.begin();
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    delay(3000);
-  }
+  
+  integerQueue = xQueueCreate(1, // Queue length
+                              sizeof(int) // Queue item size
+                              );
 
-	xTaskCreate(
-	  PCPOUT,		      // タスクの関数名
-	  "PCPOUT",	      // 名前 テキトーに
-		128,				          // 割り当てるメモリサイズ bytes
-		NULL,			            // タスクの関数に渡すパラメータへのポインタ 特にパラメータを受け取る処理は書いてないのでNULLで。
-		1,	// プライオリティ 0-20 アイドリングよりも3ほど優先させる
-	  NULL			  // タスクハンドラを渡して関連付ける
-  );
-  xTaskCreate(
-		DISPOUT,
-		"DISPOUT",
-	  512,
-		NULL,
-		2,
-		NULL
-	);
-  vTaskStartScheduler();
+  if (integerQueue != NULL) {
+    xTaskCreate(TaskAnalogRead, // Task function
+                "AnalogRead", // Task name
+                128,  // Stack size
+                NULL, 
+                1, // Priority
+                NULL);
+    xTaskCreate(TaskSignal, // Task function
+                "Signal", // Task name
+                128, // Stack size 
+                NULL, 
+                2, // Priority
+                NULL );
+  }
 }
 
-void loop(){
-  delay(1);
-/*
-  uint8_t i = 0;
-  //uint32_t pulseHigh = 500;//1700RPM
-  //uint32_t pulseLow = 2500;//1700RPM
+void TaskAnalogRead(void *pvParameters) {
+  (void) pvParameters;
 
-  //generate crank pulse
-  while(1){
-    uint16_t Value_HTL = analogRead( Volume_THL );
-    RPM = map(Value_HTL, 0, 1023, 0, 100) * 100;
+  Serial.begin(SERIAL_RATE);
+  while (!Serial) {
+    vTaskDelay(1);
+  }
 
-    pulseHigh = 10000000 / 12 / RPM;        //pulseHigh = 60 / RPM * 1000 / 12 * (5 / 30) * 1000;
-    pulseLow = 5000000 / RPM - pulseHigh;   //pulseLow = (60 / RPM * 1000 / 12) * 1000 - pulseHigh;
-  
-    Serial.print(F("rpm: "));
+  for (;;){
+    int sensorValue = analogRead(A0);
+    int RPM = map(sensorValue, 0, 1023, 1, 100) * 100;
     Serial.println(RPM);
 
-    // I2c
-    display.clearDisplay();
-    display.setTextSize(2);             // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE);        // Draw white text
-    display.setCursor(0,0);             // Start at top-left corner
-    display.print(RPM);
-    display.println(F(" rpm"));
-    display.display();
+    xQueueSend(integerQueue, &RPM, portMAX_DELAY);
 
-    for(i=0; i<12; i++){
-      if(i < 9){
-        digitalWrite(PCP, HIGH);
-      }
-      delayMicroseconds(pulseHigh);
+    // One tick delay (15ms) in between reads for stability
+    vTaskDelay(1);
+  }
+}
+
+void TaskSignal(void *pvParameters) {
+  (void) pvParameters;
+
+  pinMode(PCP, OUTPUT); //CrankPulse
+  int RPM = 0;
+  //portTickType xLastWakeTime;
+  //xLastWakeTime = xTaskGetTickCount();
+  for (;;){
+    uint8_t i = 0;
+    if (xQueueReceive(integerQueue, &RPM, portMAX_DELAY) == pdPASS) {
+      int pulseHigh = 10000 / 12 / RPM;        //pulseHigh = 60 / RPM * 1000 / 12 * (5 / 30);
+      int pulseLow = 5000 / RPM - pulseHigh;   //pulseLow = (60 / RPM * 1000 / 12) - pulseHigh;
+      
+      for(i=0; i<12; i++){
+        if(i < 9){
+          digitalWrite(PCP, HIGH);
+        }
+        //vTaskDelayUntil(&xLastWakeTime, pulseHigh / portTICK_PERIOD_MS);
+        vTaskDelay( pulseHigh / portTICK_PERIOD_MS ); // wait
  
-      if(i < 9){
-        digitalWrite(PCP, LOW);
+        if(i < 9){
+          digitalWrite(PCP, LOW);
+        }
+        //vTaskDelayUntil(&xLastWakeTime, pulseLow / portTICK_PERIOD_MS);
+        vTaskDelay( pulseLow / portTICK_PERIOD_MS ); // wait
       }
-      delayMicroseconds(pulseLow);
     }
   }
-*/
 }
+
+void loop() {}
